278e782522a2d339b6348e0b968c89eb6b0a090b
mirgafarseyidov <161619722+mirgafarseyidov@users.noreply.github.com>
2024-04-03 13:23:48 +0400

    First commit

---
+++ app.js
@@ -0,0 +1,353 @@
+// 1. Obyekt yaradin.
+// let object = {
+//     name: "Mirqafar",
+//     Surname: "Seydov",
+//     age: 29,
+//     hobbies: ["skiing", "football", "travel"],
+//     skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//      };
+// console.log(object);
+
+
+// 2. Array icinde coxlu obyektler yaradin
+
+// let students = [
+//     {
+//         name: "Farid Rustamli",
+//         Surname: "Aliyev",
+//         age: 24,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//     },
+//     {
+//         name: "Nail",
+//         surname: "Akhundov",
+//         age: 32,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+
+//     },
+//     {
+//         name: "Ulvi",
+//         surname: "Rustamli",
+//         age: 25,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//     },
+//     {
+//         name: "Nazim",
+//         surname: "Babayev",
+//         age: 33,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//     },
+//     {
+//         name: "Remzi",
+//         surname: "Yehyalı",
+//         age: 32,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//     },
+//     {
+//         name: "Nigar",
+//         surname: "Yusifli",
+//         age: 23,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//     },
+//     ]
+// console.log(students);
+
+
+
+
+// 3. Telebeler obyektinde bir nece telebe elave edin. Hobbileri, xususiyettleri kimi icerisindede deyerleri qeyd edin.
+
+// let students = [
+//     {
+//         name: "Farid Rustamli",
+//         Surname: "Aliyev",
+//         age: 24,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//         points: 95,
+//     },
+//     {
+//         name: "Nail",
+//         surname: "Akhundov",
+//         age: 32,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//         points: 85,
+
+//     },
+//     {
+//         name: "Ulvi",
+//         surname: "Rustamli",
+//         age: 25,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//         points: 40,
+//     },
+//     {
+//         name: "Nazim",
+//         surname: "Babayev",
+//         age: 33,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//         points: 55,
+//     },
+//     {
+//         name: "Remzi",
+//         surname: "Yehyalı",
+//         age: 32,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//         points: 70,
+//     },
+//     {
+//         name: "Nigar",
+//         surname: "Yusifli",
+//         age: 23,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//         points: 88,
+//     },
+//     ]
+// console.log(students);
+
+
+
+
+
+
+
+// 4. Students arrayinin qiymet ortalamasini cixardin. (Butun telebelerin qiymetlerini alib sayini bolun.)
+
+// let students = [
+//     {
+//         name: "Farid Rustamli",
+//         Surname: "Aliyev",
+//         age: 24,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//     },
+//     {
+//         name: "Nail",
+//         surname: "Akhundov",
+//         age: 32,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+
+//     },
+//     {
+//         name: "Ulvi",
+//         surname: "Rustamli",
+//         age: 25,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//     },
+//     {
+//         name: "Nazim",
+//         surname: "Babayev",
+//         age: 33,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//     },
+//     {
+//         name: "Remzi",
+//         surname: "Yehyalı",
+//         age: 32,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//     },
+//     {
+//         name: "Nigar",
+//         surname: "Yusifli",
+//         age: 23,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//     },
+//     ]
+// console.log(students);
+
+
+
+
+// 3. Telebeler obyektinde bir nece telebe elave edin. Hobbileri, xususiyettleri kimi icerisindede deyerleri qeyd edin.
+
+// let students = [
+//     {
+//         name: "Farid Rustamli",
+//         Surname: "Aliyev",
+//         age: 24,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//         points: 95,
+//     },
+//     {
+//         name: "Nail",
+//         surname: "Akhundov",
+//         age: 32,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//         points: 85,
+
+//     },
+//     {
+//         name: "Ulvi",
+//         surname: "Rustamli",
+//         age: 25,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//         points: 40,
+//     },
+//     {
+//         name: "Nazim",
+//         surname: "Babayev",
+//         age: 33,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//         points: 55,
+//     },
+//     {
+//         name: "Remzi",
+//         surname: "Yehyalı",
+//         age: 32,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//         points: 70,
+//     },
+//     {
+//         name: "Nigar",
+//         surname: "Yusifli",
+//         age: 23,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//         points: 88,
+//     },
+//     ]
+// console.log(students);
+
+// let sum = 0;
+// for (let i = 0; i < students.length; i++) {
+//   sum += students[i].points;
+// }
+// let points_ortalama = sum / students.length;
+// console.log(`Qiymət ortalaması: ${points_ortalama}`)
+
+
+// 5. Qiymeti 90-dan az olan telebeleri Ad ve Soyadi ile birlikde ekrana cixardib onlara xeberdarliq edin.
+
+
+// let students = [
+//     {
+//         name: "Farid Rustamli",
+//         Surname: "Aliyev",
+//         age: 24,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//     },
+//     {
+//         name: "Nail",
+//         surname: "Akhundov",
+//         age: 32,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+
+//     },
+//     {
+//         name: "Ulvi",
+//         surname: "Rustamli",
+//         age: 25,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//     },
+//     {
+//         name: "Nazim",
+//         surname: "Babayev",
+//         age: 33,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//     },
+//     {
+//         name: "Remzi",
+//         surname: "Yehyalı",
+//         age: 32,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//     },
+//     {
+//         name: "Nigar",
+//         surname: "Yusifli",
+//         age: 23,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//     },
+//     ]
+// console.log(students);
+
+
+
+
+// 3. Telebeler obyektinde bir nece telebe elave edin. Hobbileri, xususiyettleri kimi icerisindede deyerleri qeyd edin.
+
+// let students = [
+//     {
+//         name: "Farid Rustamli",
+//         Surname: "Aliyev",
+//         age: 24,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//         points: 95,
+//     },
+//     {
+//         name: "Nail",
+//         surname: "Akhundov",
+//         age: 32,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//         points: 85,
+
+//     },
+//     {
+//         name: "Ulvi",
+//         surname: "Rustamli",
+//         age: 25,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//         points: 40,
+//     },
+//     {
+//         name: "Nazim",
+//         surname: "Babayev",
+//         age: 33,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//         points: 55,
+//     },
+//     {
+//         name: "Remzi",
+//         surname: "Yehyalı",
+//         age: 32,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//         points: 70,
+//     },
+//     {
+//         name: "Nigar",
+//         surname: "Yusifli",
+//         age: 23,
+//         hobbies: ["skiing", "football", "travel"],
+//         skills: ["Analyzing data ", " Team leadership", " Innovation", "Imagination"],
+//         points: 88,
+//     },
+//     ]
+// console.log(students);
+
+// console.log("Qiyməti 90-dan az olan telebələr:");
+// students.forEach(student => {
+//     if (student.points < 90) {
+//         console.log(student.name, student.surname, "- Xəbərdarlıq: Qiymətiniz 90-dan aşağıdır.");
+//     }
+// });
